5dfc25070730a4a5fc351bd527a542a129642637
50051809 <50051809@belfastmet.ac.uk>
2022-07-06 16:26:14 +0100

    changes

--- ch04_java_sql/customers/src/main/java/uk/ac/serc/customers/App.java
+++ ch04_java_sql/customers/src/main/java/uk/ac/serc/customers/App.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+import java.util.ArrayList;
 /**
  * Hello world!
  *
@@ -15,13 +15,15 @@ public class App
     {
         String databaseName = "cf-training-db-server";
         String url = databaseName + ".database.windows.net";
-        String database = "customers";
+        String database = "northwind";
         String user = "cf_readonly_login";
         String password = "BDtrp7ajxtzoMpF4WN4rGDrfIDAc0xIy";
         String connectionUrl = "jdbc:sqlserver://" + url + ":1433;databaseName=" + database + ";user="+ user + ";password=" + password +";";
     
         System.out.println(connectionUrl);
 
+        ArrayList<Customer> customerList = new ArrayList<Customer>();
+        
         try {
             // Load SQL Server JDBC driver and establish connection.
             System.out.print("Connecting to SQL Server ... ");
@@ -32,8 +34,27 @@ public class App
                 try (Statement statement = connection.createStatement();
                         ResultSet resultSet = statement.executeQuery(sql)) {
                     while (resultSet.next()) {
-                        System.out.println(resultSet.getString("CustomerID") + " | " + resultSet.getString("CompanyName") + " | " + resultSet.getString("ContactName"));
+                        customerList.add(new Customer(resultSet.getString("CustomerID"), 
+                                resultSet.getString("CompanyName"),
+                                resultSet.getString("ContactName"),
+                                resultSet.getString("ContactTitle"),
+                                resultSet.getString("Address"),
+                                resultSet.getString("City"),
+                                resultSet.getString("Region"),
+                                resultSet.getString("PostalCode"),
+                                resultSet.getString("Country"),
+                                resultSet.getString("Phone"),
+                                resultSet.getString("Fax")
+                                ));
+
+                        
+                        //System.out.println(resultSet.getString("CustomerID") + " | " + resultSet.getString("CompanyName") + " | " + resultSet.getString("ContactName"));
                     }
+
+                    for(Customer customer : customerList){
+                        System.out.println(customer.toString());
+                    }
+                    
                 }
                 connection.close();
                 System.out.println("All done.");
@@ -42,6 +63,5 @@ public class App
             System.out.println();
             e.printStackTrace();
         }
-
     }
 }
